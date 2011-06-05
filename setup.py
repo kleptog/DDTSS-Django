881fd98bc831045733349e8fdd5febdcfa603d21
Martijn van Oosterhout <kleptog@svana.org>
2011-06-05 13:18:41 +0200

    - Initial commit to create project.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+#from distutils.core import setup, Extension
+
+setup(name="ddtp", version="0.1",
+      description='Debian Distributed Translation System Satellite',
+      author='Martijn van Oosterhout',
+      author_email='kleptog@svana.org',
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+      zip_safe=False,      
+      install_requires=['setuptools', 'SQLalchemy'])
+      
