dfbc1857bb68f1ad4f2ee20b4751d7098766ce92
Martijn van Oosterhout <kleptog@svana.org>
2011-06-12 23:41:14 +0200

    Updates sample script to include DDTSS.

--- src/ddtp/database/initial/make_sample.sql
+++ src/ddtp/database/initial/make_sample.sql
@@ -15,3 +15,9 @@ select distinct part_md5 into temp d_partmd5 from part_description_tb where desc
 \copy (select * from description_tb where description_id in (select description_id from d_descr)) to /tmp/description_tb.txt
 \copy (select * from package_version_tb where description_id in (select description_id from d_descr)) to /tmp/package_version_tb.txt
 \copy (select * from part_tb where part_md5 in (select part_md5 from d_partmd5)) to /tmp/part_tb.txt
+
+-- And DDTSS stuff too
+
+\copy languages_tb to /tmp/languages_tb.txt
+\copy (select * from pendingtranslations_tb where description_id in (select description_id from d_descr)) to /tmp/pendingtranslations_tb.txt
+\copy (select * from pendingtranslationreview_tb where pending_translation_id in (select pending_translation_id from pendingtranslations_tb where description_id in (select description_id from d_descr))) to /tmp/pendingtranslationreview_tb.txt
