68e3bd1e74ea1cec142b96ff48bf01f5af9f78fd
Martijn van Oosterhout <kleptog@svana.org>
2011-06-18 17:03:12 +0200

    Fix link to DDTSS.

--- src/ddtp/ddtss/urls.py
+++ src/ddtp/ddtss/urls.py
@@ -9,7 +9,7 @@ handler500 # Pyflakes
 urlpatterns = patterns(
     '',
     url(r'^$', ddtss_views.view_index, name='ddtss_index'),
-    url(r'^xx$', ddtss_views.view_index, name='ddtss_index'),
+    url(r'^xx$', ddtss_views.view_index),
     url(r'^(\w+)$', ddtss_views.view_index_lang, name='ddtss_index_lang'),
     url(r'^(\w+)/translate/(\d+)$', ddtss_views.view_translate, name='ddtss_translate'),
 )
