156f0ad9c01290e7516421867ec64db2fc0fd9cd
Martijn van Oosterhout <kleptog@svana.org>
2011-06-26 16:32:59 +0200

    Update sequences during sample loading.

--- src/ddtp/database/initial/load_sample.sql
+++ src/ddtp/database/initial/load_sample.sql
@@ -10,4 +10,12 @@ begin;
 \copy part_description_tb from part_description_tb.txt
 \copy package_version_tb from package_version_tb.txt
 
+-- Update sequences
+select setval('description_tb_description_id_seq', (select max(description_id)+1 from description_tb));
+select setval('part_tb_part_id_seq', (select max(part_id)+1 from part_tb));
+select setval('description_tag_tb_description_tag_id_seq', (select max(description_tag_id)+1 from description_tag_tb));
+select setval('translation_tb_translation_id_seq', (select max(translation_id)+1 from translation_tb));
+select setval('part_description_tb_part_description_id_seq', (select max(part_description_id)+1 from part_description_tb));
+select setval('package_version_tb_package_version_id_seq', (select max(package_version_id)+1 from package_version_tb));
+
 commit;
--- src/ddtp/database/initial/load_sample_ddtss.sql
+++ src/ddtp/database/initial/load_sample_ddtss.sql
@@ -6,4 +6,7 @@ begin;
 \copy pendingtranslations_tb from pendingtranslations_tb.txt
 \copy pendingtranslationreview_tb from pendingtranslationreview_tb.txt
 
+-- Update sequences
+select setval('pendingtranslations_tb_pending_translation_id_seq', (select max(pending_translation_id)+1 from pendingtranslations_tb));
+
 commit;
