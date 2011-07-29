b0c9a836945765c3e9602cf4c3d88b2782360bb7
Michael Bramer <grisu@deb-support.de>
2011-07-29 09:06:39 +0200

    add ddtp_web/source and ddtp_web/transdescr view

--- src/ddtp/urls.py
+++ src/ddtp/urls.py
@@ -14,7 +14,9 @@ urlpatterns = patterns(
     url(r'^$', ddt_views.view_index, name='ddt_index'),
     url(r'^(\w).html', ddt_views.view_browse, name='ddt_overview'),
     url(r'^package/([\w.+-]+)$', ddt_views.view_package, name='ddt_package'),
+    url(r'^source/([\w.+-]+)$', ddt_views.view_source, name='ddt_source'),
     url(r'^descr/(\d+)$', ddt_views.view_descr, name='ddt_descr'),
+    url(r'^descr/(\d+)-(\w+)$', ddt_views.view_transdescr, name='ddt_transdescr'),
     url(r'^ddtss/', include(ddtss_urls)),
 )
 
