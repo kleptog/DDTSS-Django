9098c0bbdc6d3f05a7280d025d30927ea45739a0
Martijn van Oosterhout <kleptog@svana.org>
2011-08-15 22:32:33 +0200

    Disable interception of redirects, it's annoying.

--- src/ddtp/development.py
+++ src/ddtp/development.py
@@ -22,3 +22,7 @@ DEBUG_TOOLBAR_PANELS = (
 #    'debug_toolbar.panels.signals.SignalDebugPanel',
     'debug_toolbar.panels.logger.LoggingPanel',
 )
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
