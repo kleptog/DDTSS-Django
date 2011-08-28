0212f506c4f288639e7db90ebcf6350a2dc9e390
Michael Bramer <grisu@deb-support.de>
2011-08-28 16:42:56 +0200

    remove homepage

--- src/ddtp/ddtp_web/views.py
+++ src/ddtp/ddtp_web/views.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2011 Martijn van Oosterhout <kleptog@svana.org>
 # See LICENCE file for details.
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.views.decorators.cache import cache_page
 from ddtp.database.ddtp import with_db_session, Description, PackageVersion, DescriptionTag, ActiveDescription, Translation, DescriptionMilestone, Part, PartDescription
@@ -43,22 +43,15 @@ def view_browse(session, request, prefix):
 @with_db_session
 def view_index(session, request):
     """ Main index.html, show summary info """
-    resultset = session.query(Translation.language, func.count(Translation.description_id), func.count(ActiveDescription.description_id)). \
-                        outerjoin(ActiveDescription, ActiveDescription.description_id == Translation.description_id). \
-                        group_by(Translation.language).order_by(Translation.language).all()
 
-    params = dict()
-    params['languages'] = [(r[0], {'total': r[1], 'active': r[2]}) for r in resultset]
-    params['translation_count'] = sum(r[1] for r in resultset)
-    params['active_count'] = sum(r[2] for r in resultset)
+    user = get_user(request, session)
+    lang = user.lastlanguage_ref
+    if lang is None:
+        lang = 'xx'
+    if lang != 'xx':
+        return redirect('ddtss_index_lang', lang)
 
-    result = session.query(func.count(Description.description_id)).one()
-    params['description_count'] = result[0]
-
-    params['prefixlist'] = map(chr, range(ord('0'), ord('9')+1))
-    params['prefixlist'] += map(chr, range(ord('a'), ord('z')+1))
-
-    return render_to_response("index.html", params, context_instance=RequestContext(request))
+    return redirect('ddtss_index')
 
 @with_db_session
 def view_package(session, request, package_name):
