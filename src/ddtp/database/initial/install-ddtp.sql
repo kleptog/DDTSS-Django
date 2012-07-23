c2438926f29dc49d0b658b52d177f0090c392b5d
Martijn van Oosterhout <kleptog@svana.org>
2012-07-23 21:14:34 +0000

    Don't set search path, user might have configured defaults.

--- src/ddtp/database/initial/install-ddtp.sql
+++ src/ddtp/database/initial/install-ddtp.sql
@@ -8,7 +8,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET escape_string_warning = off;
 
-SET search_path = public, pg_catalog;
+-- SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
 
