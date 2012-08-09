18bbd65153aa3fb78f8050ae8673f4b700be19c7
Martijn van Oosterhout <kleptog@svana.org>
2012-08-09 09:00:09 +0200

    Use table inheritence to standardise page layouts and add debian feel.

--- src/ddtp/settings.py
+++ src/ddtp/settings.py
@@ -14,11 +14,13 @@ LANGUAGE_CODE = 'en-us'
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
