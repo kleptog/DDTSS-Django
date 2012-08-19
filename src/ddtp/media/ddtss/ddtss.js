60447021efeb201a3cd69cbc600bf43b9fcd31d8
Martijn van Oosterhout <kleptog@svana.org>
2012-08-19 16:22:44 +0200

    Create a common Javascript file and move some stuff into it.

---
+++ src/ddtp/media/ddtss/ddtss.js
@@ -0,0 +1,6 @@
+// Common javascript file for the DDTSS-Django project
+function popup (url) {
+ fenster = window.open(url, "msgwindow", "width=400,height=300,resizable=yes");
+ fenster.focus();
+ return true;
+}
