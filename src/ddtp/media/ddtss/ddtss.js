e6272f29ab2f6702f5d91ec38f223d3ab80ef889
Martijn van Oosterhout <kleptog@svana.org>
2012-10-21 17:23:58 +0200

    Make messages window a bit bigger.

--- src/ddtp/media/ddtss/ddtss.js
+++ src/ddtp/media/ddtss/ddtss.js
@@ -1,6 +1,6 @@
 // Common javascript file for the DDTSS-Django project
 function popup (url) {
- fenster = window.open(url, "msgwindow", "width=400,height=300,resizable=yes");
+ fenster = window.open(url, "msgwindow", "width=400,height=600,resizable=yes");
  fenster.focus();
  return true;
 }
